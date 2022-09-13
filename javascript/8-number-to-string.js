86127a45407f281f4fffa26c52b5a8908ce78cb2
ps-gg <scherbinski.gg@gmail.com>
2022-09-13 22:49:20 +0300

    some problems

---
+++ javascript/8-number-to-string.js
@@ -0,0 +1,3 @@
+const numberToString = (n) => String(n);
+
+console.log(numberToString(10), typeof numberToString(10));
---
+++ javascript/8-string-to-number.js
@@ -0,0 +1,5 @@
+const stringToNumber = (s) => Number(s);
+
+console.log(stringToNumber(10), typeof stringToNumber(10));
+
+
