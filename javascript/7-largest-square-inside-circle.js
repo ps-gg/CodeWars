237abf1bdd52f8e79a9ac1ef9f4a5b861737d1e4
ps-gg <scherbinski.gg@gmail.com>
2022-09-15 23:02:05 +0300

    some tasks

---
+++ javascript/7-largest-square-inside-circle.js
@@ -0,0 +1,4 @@
+const areaLargestSquare = (r) => 2 * r ** 2;
+
+console.log(areaLargestSquare(15));
+
---
+++ javascript/8-convert-to-binary.js
@@ -0,0 +1,6 @@
+const toBinary = (n) => +n.toString(2);
+
+console.log(toBinary(1));
+console.log(toBinary(2));
+console.log(toBinary(3));
+console.log(toBinary(5));
