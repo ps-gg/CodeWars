7a6b76428fe3a4faf0014476c9ef1f95be21ac83
ps-gg <scherbinski.gg@gmail.com>
2022-09-17 23:04:41 +0300

    daily problems

---
+++ javascript/8-century.js
@@ -0,0 +1,8 @@
+const century = (y) => Math.ceil(y / 100);
+
+
+console.log(century(1705));
+console.log(century(1601));
+console.log(century(2000));
+console.log(century(1));
+console.log(century(101));
